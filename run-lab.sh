6d19d1b16753a7f3f58bfefd1d4b75ac498ffd7f
damian-martinezd <54448613+damian-martinezd@users.noreply.github.com>
2025-11-25 21:39:45 -0600

    Create run-lab.sh

---
+++ run-lab.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Set all variables to start de script
+PROJECT=$(gcloud config get-value project)
+BUCKET_NAME="$PROJECT-bucket"
+REGION="europe-west1"
+ZONE="us-east1-c"
+IMAGE_NAME="sample.txt"
+IMAGE_NAME_RENAMED="ada-renamed.txt"
+FOLDER_NAME="image-folder"
+
+echo "gcloud auth list and proy"
+gcloud auth list
+gcloud config list project
+gcloud config set compute/region $REGION
+
+# Create a BUCKET by command line
+echo "Create a BUCKET by command line"
+gcloud storage buckets create gs://$BUCKET_NAME
+
+# move image to test the service in Bucket via cloud
+echo "download and upload images to bucket"
+curl https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Ada_Lovelace_portrait.jpg/800px-Ada_Lovelace_portrait.jpg --output $IMAGE_NAME
+gcloud storage cp $IMAGE_NAME gs://$BUCKET_NAME
+rm $IMAGE_NAME
+
+# Download again the image from bucket to local
+gcloud storage cp -r gs://$BUCKET_NAME/$IMAGE_NAME .
+echo "image dounloaded"
+ls
+
+# Rename file on the bucket
+gsutil mv gs://$BUCKET_NAME/$IMAGE_NAME gs://$BUCKET_NAME/$IMAGE_NAME_RENAMED
+
+# List all the files on the bucket
+gcloud storage ls gs://$BUCKET_NAME
+
+# list detalles oll the files on the bicket
+gcloud storage ls -l gs://$BUCKET_NAME/$IMAGE_NAME
+
